48f93cad821f536da9c9c975b4059a35df770f11
koreanJESUS <yeahan014@gmail.com>
2014-09-24 17:34:25 -0700

    changed flexTable error

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -56,50 +56,50 @@ public class GitLab implements EntryPoint {
 	/**
 	 * This is the entry point method.
 	 */
-	public void onModuleLoad() 
+	public void onModuleLoad()
 	{
-		
+
 		service.getUsers(new AsyncCallback<List<IUser>>(){
 
 			@Override
 			public void onFailure(Throwable caught) {
 					Window.alert("Error occured " + caught.getClass() + " : " + caught.getMessage());
-				
+
 			}
 
 			@Override
 			public void onSuccess(List<IUser> result) {
 				//Window.alert("Got list back with " +  result.size() + " entries");
 				displayUsers(result);
-				
+
 			}});
-	
 
 
-		
+
+
 	}
-	
+
 	/**
-	 * Used to display users 
+	 * Used to display users
 	 * @param users
 	 */
 	public void displayUsers(List<IUser> users)
 	{
 
-		RootPanel.get("root").add("bhertfgef");
-		
+		RootPanel.get("root").add(flexTable);
+
 		flexTable.setText(0,0, "Name");
-		
+
 		flexTable.setText(0,1,"Language");
 		flexTable.setText(0,2, "Shopping Cart Size");
 		flexTable.setText(0,3, "Wish List Size");
 		flexTable.setStyleName("centered-table", true);
-		
+
 		for(int i=0; i < users.size(); i++)
 		{
-		
+
 			IUser user = users.get(i);
-			
+
 			flexTable.setText(i+1,0,user.getName());
 			if(user.getLanguage().trim().equals("EN"))
 			{
@@ -111,9 +111,9 @@ public class GitLab implements EntryPoint {
 			{
 				flexTable.setText(i+1,1,user.getLanguage());
 			}
-			
+
 			flexTable.setText(i+1,2,String.valueOf(user.getShoppingCart().size()));
-			
+
 			flexTable.setText(i+1,3,String.valueOf(user.getWishList().size()));
 		}
 
